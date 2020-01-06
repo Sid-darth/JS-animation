89c0cf942e37bd8893b0aacab1afb81c579221ca
Sid-darth <realsiddarth@gmail.com>
2020-01-06 01:52:18 -0800

    added basic logic for a Count Down timer

--- js/logic.js
+++ js/logic.js
@@ -1,3 +1,20 @@
+var timer = null;
+var countDownNumber = 10;
+
 var changeState = function(state) {
 	document.body.className = "body-state"+state;
+
+	if(state==2){
+		timer = setInterval(function(){
+			if (countDownNumber>0) {
+			document.getElementById('countDown').innerHTML 
+			= countDownNumber;
+			countDownNumber -=1;
+		} else{
+			state = 3;
+			changeState(state);
+		}
+		}, 1000);
+		
+	}
 }
